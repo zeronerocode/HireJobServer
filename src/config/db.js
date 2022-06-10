e6bce03803b5de4074eb37a07d9ce5ce43ffd1af
hasbialwikusmana <hasbialwi70@gmail.com>
2022-06-10 19:02:38 +0700

    first commit backend hire job

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+pool.connect((err) => {
+  if (err) {
+    console.log("Database not connected");
+  } else {
+    console.log(`Database connected on ${process.env.DB_NAME}`);
+  }
+});
+
+module.exports = pool;
---
+++ src/helper/response.js
@@ -0,0 +1,12 @@
+const response = (res, result, status, message, pagination) => {
+  const resultPrint = {};
+  resultPrint.statuCode = status;
+  resultPrint.data = result;
+  resultPrint.message = message || null;
+  if (pagination) resultPrint.pagination = pagination;
+  res.status(status).json(resultPrint);
+};
+
+module.exports = {
+  response,
+};
