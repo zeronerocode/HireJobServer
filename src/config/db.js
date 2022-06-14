447c508d24c71bc1d48c2c0f565767a1172acf14
Akmal F <zeronerocode@gmail.com>
2022-06-14 22:00:52 +0700

    add ssl

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,9 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+}
 });
 pool.connect((err) => {
   if (err) {
